cc2878f25850cf2d99bebd1fccf201fa4aa27ed2
bluesolar1999 <blue.solar1999@gmail.com>
2021-01-04 10:10:30 +0900

    ユーザーの編集ページ作成、ユーザーの更新処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params_update)
+      flash[:success] = "プロフィールが更新されました！"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-      end
+    end
+
+    def user_params_update
+      params.require(:user).permit(:name, :email, :introduction, :sex)
+    end
 end
